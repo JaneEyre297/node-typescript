5287a67acf3597b53b541ff93cf5aeee39bceee6
刘思颖 <liusiying@baicizhan.com>
2020-07-20 10:09:48 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ const program = require('commander');
 program
   .version('1.0.0')
   .option('-p --path [path]', '上传指定目录资源到CDN')
-  .option('-f --file <filename>', '上传指定资源到CDN');
+  .option('-f --file <filename>', '上传指定资源到CDN')
+  .command('push')
+  .action(function() {
+      console.log(222)
+  })
 
 program.parse(process.argv);
 
